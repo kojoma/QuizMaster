58aca0f3b47c57f96d11cf5ebba8366c8ca89bbc
kojoma <masaru.kojo@gmail.com>
2018-07-17 19:52:21 +0900

    display raw question content

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ApplicationRecord
   has_one :answer, dependent: :destroy
   accepts_nested_attributes_for :answer
+
   validates :content, presence: true
+  validates :content, format: { without: /<script>/ }
+  validates :content, format: { without: /<\/script>/ }
+  validates :content, format: { without: /javascript:/ }
+
   validates_associated :answer
 end
