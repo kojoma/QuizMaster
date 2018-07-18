23cec316caba94c33df688eb89a81749759032f4
kojoma <masaru.kojo@gmail.com>
2018-07-18 16:40:45 +0900

    convert english when quiz scoring

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -29,16 +29,96 @@ describe Question do
   end
 
   describe 'is_correct?' do
-    let!(:question) { create(:question) }
-    let!(:answer) { create(:answer, question_id: question.id) }
-    before { question.reload }
+    describe 'should return true when answer correct' do
+      it "answer = question.answer.content" do
+        question = create(:question)
+        answer = create(:answer, question_id: question.id)
+        question.reload
 
-    it "should return true when correct" do
-      expect(question.is_correct?(answer.content)).to be_truthy
+        expect(question.is_correct?(answer.content)).to be_truthy
+      end
+
+      it "answer english = question.answer.content" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '5'})
+        question.reload
+
+        expect(question.is_correct?('five')).to be_truthy
+      end
+
+      it "answer = question.answer.content english" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: 'five'})
+        question.reload
+
+        expect(question.is_correct?('5')).to be_truthy
+      end
+
+      it "answer = question.answer.content english sentence" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '5 sentence'})
+        question.reload
+
+        expect(question.is_correct?('five sentence')).to be_truthy
+      end
+
+      it "answer english = question.answer.content, 2 digit number" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '55'})
+        question.reload
+
+        expect(question.is_correct?('fifty-five')).to be_truthy
+      end
+
+      it "answer english = question.answer.content, 3 digit number" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '555'})
+        question.reload
+
+        expect(question.is_correct?('five hundred and fifty-five')).to be_truthy
+      end
     end
 
-    it "should return false when incorrect" do
-      expect(question.is_correct?(answer.content + 'incorrect')).to be_falsey
+    describe 'should return false when answer incorrect' do
+      it "answer != question.answer.content" do
+        question = create(:question)
+        answer = create(:answer, question_id: question.id)
+        question.reload
+
+        expect(question.is_correct?(answer.content + 'incorrect')).to be_falsey
+      end
+
+      it "answer english != question.answer.content" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '5'})
+        question.reload
+
+        expect(question.is_correct?('six')).to be_falsey
+      end
+
+      it "answer != question.answer.content english" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: 'five'})
+        question.reload
+
+        expect(question.is_correct?('6')).to be_falsey
+      end
+
+      it "answer != question.answer.content english sentence" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '5 sentence'})
+        question.reload
+
+        expect(question.is_correct?('six sentence')).to be_falsey
+      end
+
+      it "answer english = question.answer.content, but to_en out of range" do
+        question = create(:question)
+        answer = create(:answer, {question_id: question.id, content: '1000'})
+        question.reload
+
+        expect(question.is_correct?('one thousand')).to be_falsey
+      end
     end
   end
 end
